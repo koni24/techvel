a9f4fd75cb0efa70ad8118ef6bfd2be761566a15
koni24 <73066305+koni24@users.noreply.github.com>
2022-03-11 22:59:05 +0530

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Key } from 'react'
 import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
 
 
-export default function Home({ posts }) {
+export default function Home({ posts }:{posts:any}) {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
@@ -14,13 +15,11 @@ export default function Home({ posts }) {
       </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post) => (
-            <PostCard post={post.node} key={post.title} />
-          ))}
+{posts.map((post: { node: any; title: Key | null | undefined }) => <PostCard post={post.node} key={post.title} />  )}
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative top-8 lg:sticky">
-            <PostWidget />
+            <PostWidget categories={undefined} slug={undefined} />
             <Categories />
           </div>
         </div>
